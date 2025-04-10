5b7fed7c135457596fb06e1055204d859af03759
Mary <moyseyivna@gmail.com>
2025-04-10 16:50:20 +0300

    HW

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import {FamilyComponent} from "./components/family-component/FamilyComponent.tsx";
 
 function App() {
 
 
   return (
     <>
-
+        <FamilyComponent/>
     </>
   )
 }
