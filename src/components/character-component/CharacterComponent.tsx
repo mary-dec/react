1ff4b668e31c2c4ff6f3339e55fec9083b763033
Mary <moyseyivna@gmail.com>
2025-04-10 15:37:19 +0300

    HW

--- src/components/character-component/CharacterComponent.tsx
+++ src/components/character-component/CharacterComponent.tsx
@@ -1,14 +1,15 @@
 import {ICharacter} from "../../models/ICharacter.ts";
-import {ReactNode, reactNode} from "react";
+import {ReactNode} from "react";
 interface CharacterComponentProps {
     item: ICharacter,
     children: ReactNode,
 }
 
-export const CharacterComponent = () => {
-    return (
+export const CharacterComent = ({item, children}: CharacterComponentProps)
+   return (
         <div>
                 <h3>{item.name} {item.surname}</h3>
+                <p>children</p>
         </div>
     );
-};
+
